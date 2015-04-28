8daaaf81384256e8b932bcf2ecdee2fa96caa2ec
Christian Svensson <bluecmd@google.com>
2015-04-28 16:24:06 +0100

    Re-open tab on close instead of having a confirmation dialog

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,34 +1,20 @@
-pinnedTabs = {};
-
-function patchTab(tabId){
-    chrome.tabs.executeScript(tabId, {code: "window.onbeforeunload = function(e) { return 'This is a pinned tab'; };"});
-};
-
-chrome.tabs.onCreated.addListener(function(tab){
-  if(tab.pinned) {
-    pinnedTabs[tab.id] = true;
-    patchTab(tab.id);
-  }
-});
+tabs = {};
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if(changeInfo && changeInfo.pinned) {
-    // Check to see if the tab that got updates wasn't already pinned
-    if(!pinnedTabs[tabId]){
-      pinnedTabs[tabId] = true;
-      patchTab(tabId);
-    }
-  } else if (changeInfo && !changeInfo.pinned) {
-    // Check to see if the tab that got updates was pinned
-    if(pinnedTabs[tabId]){
-      delete pinnedTabs[tabId];
-      chrome.tabs.executeScript(tabId, {code: "window.onbeforeunload = null;"});
-    }
-  }
+  tabs[tabId] = tab;
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-  if(pinnedTabs[tabId]){
-    delete pinnedTabs[tabId];
+  if(tabs[tabId] && tabs[tabId].pinned) {
+    var tab = tabs[tabId];
+    delete tabs[tabId];
+    chrome.tabs.create({
+      pinned: true,
+      url: tab.url,
+      active: true,
+      index: tab.index
+    }, function(newTab) {
+      tabs[newTab.id] = newTab;
+    });
   }
 });
