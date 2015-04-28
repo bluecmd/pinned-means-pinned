d030c6b2ef0b1b81dc97fcbc0ca980271364d9f8
Christian Svensson <bluecmd@google.com>
2015-04-28 16:27:14 +0100

    Allow closing on window close

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,9 +5,17 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-  if(tabs[tabId] && tabs[tabId].pinned) {
+  // Only allow closing if the window is closing down
+  if (removeInfo.isWindowClosing) {
+    return;
+  }
+
+  // Otherwise, re-open the tab if it's pinned
+  if (tabs[tabId] && tabs[tabId].pinned) {
     var tab = tabs[tabId];
     delete tabs[tabId];
+    // Chrome doesn't allow us from stopping a removal AFAIK, so just re-open
+    // the tab in the same place as a pinned tab
     chrome.tabs.create({
       pinned: true,
       url: tab.url,
